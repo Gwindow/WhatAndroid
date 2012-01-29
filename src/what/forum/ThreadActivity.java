68a8b4a9eff09e10487dfc105d547b7fd39bf1ea
Gwindow <tim.pirate@gmail.com>
2012-01-28 18:10:18 -0800

    added reply button to threads

--- src/what/forum/ThreadActivity.java
+++ src/what/forum/ThreadActivity.java
@@ -6,17 +6,21 @@ import java.util.List;
 import what.gui.ImageLoader;
 import what.gui.MyActivity;
 import what.gui.R;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -34,7 +38,7 @@ public class ThreadActivity extends MyActivity implements OnLongClickListener {
 	private Intent intent;
 	private int id, page, postId;
 	private TextView threadTitle;
-	private Button backButton, nextButton, lastButton;
+	private Button backButton, nextButton, lastButton, replyButton;
 	private ArrayList<RelativeLayout> listOfPosts = new ArrayList<RelativeLayout>();
 
 	@Override
@@ -47,9 +51,12 @@ public class ThreadActivity extends MyActivity implements OnLongClickListener {
 		backButton = (Button) this.findViewById(R.id.previousButton);
 		nextButton = (Button) this.findViewById(R.id.nextButton);
 		lastButton = (Button) this.findViewById(R.id.lastButton);
+		replyButton = (Button) this.findViewById(R.id.replyButton);
+
 		setButtonState(backButton, false);
 		setButtonState(nextButton, false);
 		setButtonState(lastButton, false);
+		setButtonState(replyButton, false);
 
 		getBundle();
 
@@ -79,6 +86,7 @@ public class ThreadActivity extends MyActivity implements OnLongClickListener {
 		setButtonState(backButton, thread.hasPreviousPage());
 		setButtonState(nextButton, thread.hasNextPage());
 		setButtonState(lastButton, thread.hasNextPage());
+		setButtonState(replyButton, !thread.getResponse().isLocked());
 		threadTitle.setText(thread.getResponse().getThreadTitle() + ", page " + thread.getResponse().getCurrentPage());
 		List<Posts> posts = thread.getResponse().getPosts();
 		RelativeLayout layout;
@@ -290,6 +298,80 @@ public class ThreadActivity extends MyActivity implements OnLongClickListener {
 				Toast.makeText(ThreadActivity.this, "Could not load thread", Toast.LENGTH_LONG).show();
 			}
 			unlockScreenRotation();
+		}
+	}
+
+	public void reply(View v) {
+		replyDialog();
+	}
+
+	private void replyDialog() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+		alert.setTitle("");
+		alert.setMessage("Reply");
+
+		final EditText input = new EditText(this);
+		input.setGravity(Gravity.TOP);
+		input.setGravity(Gravity.LEFT);
+		input.setMinHeight(this.getHeight() / 3);
+		input.setMinWidth(this.getWidth() / 2);
+		input.setText(QuoteBuffer.getBuffer());
+		alert.setView(input);
+
+		alert.setPositiveButton("Post", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+				if (input.getText().length() > 0) {
+					new PostReply().execute(input.getText().toString());
+				} else {
+					Toast.makeText(ThreadActivity.this, "Enter a reply", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+
+		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+
+			}
+		});
+
+		alert.show();
+	}
+
+	private class PostReply extends AsyncTask<String, Void, Boolean> {
+		@Override
+		protected void onPreExecute() {
+			lockScreenRotation();
+			dialog = new ProgressDialog(ThreadActivity.this);
+			dialog.setIndeterminate(true);
+			dialog.setMessage("Loading...");
+			dialog.show();
+		}
+
+		@Override
+		protected Boolean doInBackground(String... params) {
+			try {
+				Thread.postReply(id, params[0]);
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+
+		@Override
+		protected void onPostExecute(Boolean status) {
+			dialog.dismiss();
+			if (status == true) {
+				Toast.makeText(ThreadActivity.this, "Reply posted", Toast.LENGTH_SHORT).show();
+			}
+			if (status == false) {
+				Toast.makeText(ThreadActivity.this, "Could not post reply", Toast.LENGTH_LONG).show();
+			}
+			unlockScreenRotation();
+			finish();
 		}
 	}
 }
