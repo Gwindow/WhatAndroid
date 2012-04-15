cf3fbd1fad1046ce7548b8841e4cfa7c81c9d52e
Gwindow <tim.pirate@gmail.com>
2012-04-14 23:25:32 -0700

    margins and paddings changed, tablet scaling, minor stuff

--- src/what/login/WhatAndroidActivity.java
+++ src/what/login/WhatAndroidActivity.java
@@ -42,7 +42,7 @@ import api.util.Updater;
 
 public class WhatAndroidActivity extends MyActivity implements OnClickListener {
 	// TODO remove
-	private final static double VERSION = 0.30;
+	private final static double VERSION = 0.31;
 	private static String SITE = "ssl.what.cd";
 	private final static String UPDATE_SITE = "https://raw.github.com/Gwindow/WhatAndroid/gh-pages/index.html";
 	public static double INSTALLED_VERSION;
