891e5dc8f01331c9d0011d42790583635007584a
Gwindow <tim.pirate@gmail.com>
2012-06-08 01:09:00 -0700

    thread_post.xml

---
+++ src/what/forum/ThreadActivity.java
@@ -0,0 +1,29 @@
+package what.forum;
+
+import what.gui.MyActivity2;
+
+/**
+ * @author Gwindow
+ * @since Jun 8, 2012 1:08:36 AM
+ */
+public class ThreadActivity extends MyActivity2 {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
