3f55fad3db7cdd2a41f5044ee578de453ac5dc80
Will <willusher.life@gmail.com>
2013-08-05 17:46:49 -0700

    Adding ability to distinguish dev/beta releases and regular releases by their version names
    a preview/beta build will be named like x.x.x.b while releases are
    x.x.x, preview builds are counted as lower than a release with the same
    version name so we'll notify if the preview version we're running has
    been officially released to update to the released version

--- src/what/util/VersionNumber.java
+++ src/what/util/VersionNumber.java
@@ -5,11 +5,13 @@ package what.util;
  */
 public class VersionNumber {
 	private int major = 0, minor = 0, patch = 0;
+	private boolean preview = false;
 
 	/**
 	 * Create the version number, the string should contain the version information
 	 * such as "1.2.1"
 	 * Will also handle cases of incomplete version numbers, ie. 1.2 will be read to 1.2.0
+	 * A version number with a 'b' at the end indicates a preview(beta) build: 1.2.1.b -> 1.2.1 beta release
 	 * @param s string containg the version information
 	 */
 	public VersionNumber(String s){
@@ -20,6 +22,8 @@ public class VersionNumber {
 			minor = Integer.parseInt(vals[1]);
 		if (vals.length > 2)
 			patch = Integer.parseInt(vals[2]);
+		if (s.charAt(s.length() - 1) == 'b')
+			preview = true;
 	}
 
 	/**
@@ -28,11 +32,15 @@ public class VersionNumber {
 	 * @return true if this version is a higher version
 	 */
 	public boolean isHigher(VersionNumber other){
-		if (major > other.major)
-			return true;
-		if (major == other.major && minor > other.minor)
-			return true;
-		return (major == other.major && minor == other.minor && patch > other.patch);
+		if (major < other.major)
+			return false;
+		if (minor < other.minor)
+			return false;
+		if (patch < other.patch)
+			return false;
+		//If the version numbers are equal but we aren't a preview and the other build is, we're higher
+		return !(major == other.major && minor == other.minor && patch == other.patch)
+			|| (!preview && other.preview);
 	}
 
 	public int getMajor(){
@@ -47,8 +55,12 @@ public class VersionNumber {
 		return patch;
 	}
 
+	public boolean isPreview(){
+		return preview;
+	}
+
 	@Override
 	public String toString(){
-		return major + "." + minor + "." + patch;
+		return major + "." + minor + "." + patch + (preview ? " preview" : "");
 	}
 }
