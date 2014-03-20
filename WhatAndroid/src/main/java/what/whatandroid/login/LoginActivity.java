a4c4ada3b496cd92d37960e8ad2f65da8e6b6007
Will <willusher.life@gmail.com>
2014-03-20 16:28:00 -0700

    Add login progress dialog to login activity.

--- WhatAndroid/src/main/java/what/whatandroid/login/LoginActivity.java
+++ WhatAndroid/src/main/java/what/whatandroid/login/LoginActivity.java
@@ -1,6 +1,7 @@
 package what.whatandroid.login;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -31,7 +32,8 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 		login.setOnClickListener(this);
 		//TODO: Developers put your local Gazelle install IP here instead of testing on the live site
 		//I recommend setting up with Vagrant: https://github.com/dr4g0nnn/VagrantGazelle
-		MySoup.setSite("192.168.1.3:8080", false);
+		MySoup.setSite("192.168.1.125:8080", false);
+		//MySoup.setSite("what.cd", true);
 
 		//Setup Universal Image loader global config
 		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
@@ -45,12 +47,11 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 	public void onClick(View v) {
 		//The only thing being listened to for clicks in the view is the login button, so skip checking who was clicked
 		if (username.length() > 0 && password.length() > 0){
-			//TODO: Should show a progress dialog and proper error dialog instead of Toasts
-			Toast.makeText(this, "Logging you in, " + username.getText().toString(), Toast.LENGTH_LONG).show();
 			new Login().execute(username.getText().toString(), password.getText().toString());
 		}
 		else {
 			Toast.makeText(this, "Please enter your username and password", Toast.LENGTH_SHORT).show();
+			username.requestFocus();
 		}
 	}
 
@@ -59,6 +60,8 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 	 * into the site via the api library
 	 */
 	private class Login extends AsyncTask<String, Void, Boolean> {
+		private ProgressDialog dialog;
+
 		@Override
 		protected Boolean doInBackground(String... params) {
 			String name = params[0];
@@ -73,9 +76,17 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 		}
 
 		@Override
+		protected void onPreExecute(){
+			dialog = new ProgressDialog(LoginActivity.this);
+			dialog.setIndeterminate(true);
+			dialog.setMessage("Logging in...");
+			dialog.show();
+		}
+
+		@Override
 		protected void onPostExecute(Boolean status) {
+			dialog.dismiss();
 			if (status){
-				Toast.makeText(LoginActivity.this, "Login success", Toast.LENGTH_SHORT).show();
 				Intent intent = new Intent(LoginActivity.this, AnnouncementsActivity.class);
 				startActivity(intent);
 			}
