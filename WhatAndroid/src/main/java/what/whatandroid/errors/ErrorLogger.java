6d217d4fa93b8dca2c2027c4d2bea8e4b4218e0c
Will <willusher.life@gmail.com>
2014-03-24 12:13:15 -0700

    Adding error logging and reporting
    The error logger catches uncaught exceptions, dumps an error report then passes the exception on to the original uncaught exception handler. The error reporter service is run on login and checks for any crash report files. If crash report files are found the user is given a notification to send the reports to the devs.

---
+++ WhatAndroid/src/main/java/what/whatandroid/errors/ErrorLogger.java
@@ -0,0 +1,104 @@
+package what.whatandroid.errors;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.os.Build;
+
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Handles checking for any errors that may have crashed the app previously
+ * and listening for uncaught errors and writing out stack traces that we
+ * can then pick up and report the next time the app starts
+ */
+public class ErrorLogger implements Thread.UncaughtExceptionHandler {
+	/**
+	 * The previous error handler so that we can throw the exception up once we've
+	 * written our crash report
+	 */
+	private Thread.UncaughtExceptionHandler prevHandler;
+	private Context context;
+
+	/**
+	 * Create an error reporter to check for errors. The context will be used to launch a send to
+	 * intent if an error report is found
+	 *
+	 * @param context context to send error report in
+	 */
+	public ErrorLogger(Context context){
+		this.context = context;
+		prevHandler = Thread.getDefaultUncaughtExceptionHandler();
+	}
+
+	@Override
+	public void uncaughtException(Thread thread, Throwable ex){
+		saveReport(buildReport(thread, ex));
+		prevHandler.uncaughtException(thread, ex);
+	}
+
+	/**
+	 * Save the error report to disk using some generated filename and the .report extension
+	 *
+	 * @param report error report to save
+	 */
+	private void saveReport(String report){
+		try {
+			Random rand = new Random();
+			String fName = rand.nextInt() + ".report";
+			FileOutputStream file = context.openFileOutput(fName, Context.MODE_PRIVATE);
+			file.write(report.getBytes());
+			file.close();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Get the error report for this crash
+	 */
+	private String buildReport(Thread thread, Throwable ex){
+		StringBuilder report = new StringBuilder();
+		try {
+			PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+			report.append("Error Report on: ")
+				.append(new Date().toString())
+				.append("\n")
+				.append(info.packageName)
+				.append(" version name: ")
+				.append(info.versionName)
+				.append(", version code: ")
+				.append(info.versionCode)
+				.append("\nAndroid Version: ")
+				.append(Build.VERSION.RELEASE)
+				.append("\nDevice: ")
+				.append(Build.DEVICE)
+				.append("\nBrand: ")
+				.append(Build.BRAND)
+				.append("\nModel: ")
+				.append(Build.MODEL);
+
+			Writer writer = new StringWriter();
+			PrintWriter printWriter = new PrintWriter(writer);
+			ex.printStackTrace(printWriter);
+			report.append("\n\nStack Trace:\n")
+				.append(writer.toString())
+				.append("\n\nException Cause:\n");
+
+			for (Throwable cause = ex.getCause(); cause != null; cause = cause.getCause()){
+				cause.printStackTrace(printWriter);
+				report.append(writer);
+			}
+			printWriter.close();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		return report.toString();
+	}
+}
---
+++ WhatAndroid/src/main/java/what/whatandroid/errors/ErrorReporterService.java
@@ -0,0 +1,86 @@
+package what.whatandroid.errors;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import org.apache.commons.io.IOUtils;
+import what.whatandroid.R;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+
+/**
+ * Service to search the filesystem for existing error reports and push a notification to
+ * email the reports to the devs
+ */
+public class ErrorReporterService extends IntentService {
+
+	public ErrorReporterService(){
+		super("WhatAndroid-ErrorReporterService");
+	}
+
+	/**
+	 * Check the system for an crash reports and build up a report containing all of them
+	 * then push a notification to the user that there are reports available to send
+	 */
+	@Override
+	protected void onHandleIntent(Intent intent){
+		StringBuilder reports = findReports();
+		if (reports.length() != 0){
+			NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+			builder.setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("WhatAndroid Crash Reports Found")
+				.setContentText("Whoops! Please click to send the reports to the devs")
+				.setAutoCancel(true);
+
+			Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "whatcdandroid@gmail.com", null));
+			emailIntent.putExtra(Intent.EXTRA_SUBJECT, "WhatAndroid Crash Report");
+			emailIntent.putExtra(Intent.EXTRA_TEXT, reports.toString());
+			emailIntent = Intent.createChooser(emailIntent, "Send crash reports");
+			PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, emailIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			builder.setContentIntent(pendingIntent);
+
+			NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+			manager.notify(0, builder.build());
+		}
+	}
+
+	/**
+	 * Search the system for error reports and delete the crash files
+	 *
+	 * @return the built string containing all found error reports
+	 */
+	private StringBuilder findReports(){
+		StringBuilder reports = new StringBuilder();
+		if (getFilesDir() == null){
+			return reports;
+		}
+		try {
+			FilenameFilter filter = new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String filename){
+					return filename.endsWith(".report");
+				}
+			};
+			for (File f : getFilesDir().listFiles(filter)){
+				BufferedReader reader = new BufferedReader(new FileReader(f));
+				reports.append(IOUtils.toString(reader))
+					.append("\n\n");
+				//Clean up error reports
+				if (!f.delete()){
+					System.err.println("Failed to delete file: " + f.getName());
+				}
+			}
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		return reports;
+	}
+}
