0689a011020fcc58aa149b37ac0f0a14ea0d4783
Will <willusher.life@gmail.com>
2014-03-25 15:47:13 -0700

    Make the settings and send feedback options available in the login activity as well

--- WhatAndroid/src/main/java/what/whatandroid/login/LoginActivity.java
+++ WhatAndroid/src/main/java/what/whatandroid/login/LoginActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.IntentCompat;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +19,7 @@ import api.son.MySon;
 import api.soup.MySoup;
 import what.whatandroid.R;
 import what.whatandroid.profile.ProfileActivity;
+import what.whatandroid.settings.SettingsActivity;
 import what.whatandroid.settings.SettingsFragment;
 
 import java.net.HttpCookie;
@@ -87,6 +91,31 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 			Toast.makeText(this, "Please enter your username and password", Toast.LENGTH_SHORT).show();
 			username.requestFocus();
 		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		Intent intent;
+		switch (item.getItemId()){
+			case R.id.action_settings:
+				intent = new Intent(this, SettingsActivity.class);
+				startActivity(intent);
+				return true;
+			case R.id.action_feedback:
+				intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "whatcdandroid@gmail.com", null));
+				intent.putExtra(Intent.EXTRA_SUBJECT, "WhatAndroid Feedback");
+				startActivity(Intent.createChooser(intent, "Send email"));
+				return true;
+			default:
+				break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	/**
