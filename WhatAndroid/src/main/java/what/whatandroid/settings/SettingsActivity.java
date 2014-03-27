1804bacb2bf38c0f56588ae89d6a34d6209a26a7
Will <willusher.life@gmail.com>
2014-03-26 18:29:30 -0700

    Add setting to disable downloading of images when not on wifi

--- WhatAndroid/src/main/java/what/whatandroid/settings/SettingsActivity.java
+++ WhatAndroid/src/main/java/what/whatandroid/settings/SettingsActivity.java
@@ -1,7 +1,13 @@
 package what.whatandroid.settings;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import what.whatandroid.R;
 
 /**
  * Activity for changing user settings
@@ -14,5 +20,22 @@ public class SettingsActivity extends Activity {
 		getFragmentManager().beginTransaction()
 			.replace(android.R.id.content, new SettingsFragment())
 			.commit();
+	}
+
+	/**
+	 * See if images are enabled for the context. We also cache the result so we don't need to do the
+	 * check so often
+	 *
+	 * @return true if enabled, false if not
+	 */
+	public static Boolean imagesEnabled(Context context){
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		boolean wifiOnly = preferences.getBoolean(context.getString(R.string.key_pref_img_wifi), true);
+		if (wifiOnly){
+			ConnectivityManager manager = (ConnectivityManager)context.getSystemService(CONNECTIVITY_SERVICE);
+			NetworkInfo wifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+			return wifi.isConnected();
+		}
+		return true;
 	}
 }
