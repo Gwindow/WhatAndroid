0052e32a4dabc133ae27de42d56d216767aaf26e
Will <willusher.life@gmail.com>
2014-04-14 15:33:37 -0700

    Merge simple tag handling into one function since it's all the same but with different styles to apply

--- WhatAndroid/src/main/java/what/whatandroid/comments/WhatBBParser.java
+++ WhatAndroid/src/main/java/what/whatandroid/comments/WhatBBParser.java
@@ -2,7 +2,10 @@ package what.whatandroid.comments;
 
 import android.graphics.Typeface;
 import android.text.SpannableStringBuilder;
+import android.text.style.CharacterStyle;
+import android.text.style.StrikethroughSpan;
 import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 
 /**
  * Takes a bb formatted string and builds a formatted Spanned to display the text
@@ -11,47 +14,39 @@ public class WhatBBParser {
 	/**
 	 * Tags for opening and closing a bold section of text
 	 */
-	private static final String[] BOLD = {"[b]", "[/b]"}, ITALIC = {"[i]", "[/i]"};
+	private static final String[] BOLD = {"[b]", "[/b]"}, ITALIC = {"[i]", "[/i]"}, UNDERLINE = {"[u]", "[/u]"},
+		STRIKETHROUGH = {"[s]", "[/s]"};
 
 	public static CharSequence parsebb(String bbText){
 		SpannableStringBuilder ssb = new SpannableStringBuilder(bbText);
 		StringBuilder text = new StringBuilder(bbText);
-		parseBold(ssb, text);
-		parseItalic(ssb, text);
+		parseSimpleTag(ssb, text, BOLD, new StyleSpan(Typeface.BOLD));
+		parseSimpleTag(ssb, text, ITALIC, new StyleSpan(Typeface.ITALIC));
+		parseSimpleTag(ssb, text, UNDERLINE, new UnderlineSpan());
+		parseSimpleTag(ssb, text, STRIKETHROUGH, new StrikethroughSpan());
 		return ssb;
 	}
 
 	/**
-	 * Parse the bold tags in the string and apply formatting to them
+	 * Parse a simple tag passed and apply the styling desired. A simple tag is bold, italic, underline or similar
+	 * where no extra information from the tag is required to decide the formatting.
+	 *
+	 * @param ssb  spannable string builder to apply the styling in
+	 * @param text a mirror of the text in the spannable string builder, used to look up tag positions and tags will be
+	 *             removed in here and in the ssb to keep them matching
+	 * @param tag  tag[0] is the opening tag, tag[1] is the closing tag
+	 * @param cs   character style to apply to the text in the tag
 	 */
-	private static void parseBold(SpannableStringBuilder ssb, StringBuilder text){
-		//Maybe pass this builder around everywhere as well? Less copying
-		for (int s = text.indexOf(BOLD[0]) + BOLD[0].length(), e = text.indexOf(BOLD[1], s); s != -1 && e != -1;
-			 s = text.indexOf(BOLD[0], s) + BOLD[0].length(), e = text.indexOf(BOLD[1], s)){
-			ssb.setSpan(new StyleSpan(Typeface.BOLD), s, e, 0);
-			//Remove the open and close tags
-			ssb.delete(s - BOLD[0].length(), s);
-			text.delete(s - BOLD[0].length(), s);
-			e -= BOLD[0].length();
-			ssb.delete(e, e + BOLD[1].length());
-			text.delete(e, e + BOLD[1].length());
-		}
-	}
-
-	/**
-	 * Parse the italic tags in the string and apply formatting to them
-	 */
-	private static void parseItalic(SpannableStringBuilder ssb, StringBuilder text){
-		//Maybe pass this builder around everywhere as well? Less copying
-		for (int s = text.indexOf(ITALIC[0]) + ITALIC[0].length(), e = text.indexOf(ITALIC[1], s); s != -1 && e != -1;
-			 s = text.indexOf(ITALIC[0], s) + ITALIC[0].length(), e = text.indexOf(ITALIC[1], s)){
-			ssb.setSpan(new StyleSpan(Typeface.ITALIC), s, e, 0);
+	private static void parseSimpleTag(SpannableStringBuilder ssb, StringBuilder text, String tag[], CharacterStyle cs){
+		for (int s = text.indexOf(tag[0]) + tag[0].length(), e = text.indexOf(tag[1], s); s != -1 && e != -1;
+			 s = text.indexOf(tag[0], s) + tag[0].length(), e = text.indexOf(tag[1], s)){
+			ssb.setSpan(CharacterStyle.wrap(cs), s, e, 0);
 			//Remove the open and close tokens
-			ssb.delete(s - ITALIC[0].length(), s);
-			text.delete(s - ITALIC[0].length(), s);
-			e -= ITALIC[0].length();
-			ssb.delete(e, e + ITALIC[1].length());
-			text.delete(e, e + ITALIC[1].length());
+			ssb.delete(s - tag[0].length(), s);
+			text.delete(s - tag[0].length(), s);
+			e -= tag[0].length();
+			ssb.delete(e, e + tag[1].length());
+			text.delete(e, e + tag[1].length());
 		}
 	}
 }
