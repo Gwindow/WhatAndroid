1d2dc4cf9676fbaafb9b39109584f3cd9cd3a8ed
Will <willusher.life@gmail.com>
2014-04-14 16:32:00 -0700

    Start working on support for parameterized tags like align, color, urls, etc.

--- WhatAndroid/src/main/java/what/whatandroid/comments/WhatBBParser.java
+++ WhatAndroid/src/main/java/what/whatandroid/comments/WhatBBParser.java
@@ -1,6 +1,7 @@
 package what.whatandroid.comments;
 
 import android.graphics.Typeface;
+import android.text.Layout;
 import android.text.SpannableStringBuilder;
 import android.text.style.*;
 
@@ -13,7 +14,7 @@ public class WhatBBParser {
 	 */
 	private static final String[] BOLD = {"[b]", "[/b]"}, ITALIC = {"[i]", "[/i]"}, UNDERLINE = {"[u]", "[/u]"},
 		STRIKETHROUGH = {"[s]", "[/s]"}, IMPORTANT = {"[important]", "[/important]"}, CODE = {"[code]", "[/code]"},
-		PRE = {"[pre]", "[/pre]"};
+		PRE = {"[pre]", "[/pre]"}, ALIGN = {"[align=", "[/align]"};
 
 	public static CharSequence parsebb(String bbText){
 		SpannableStringBuilder ssb = new SpannableStringBuilder(bbText);
@@ -25,6 +26,7 @@ public class WhatBBParser {
 		parseSimpleTag(ssb, text, IMPORTANT, new ForegroundColorSpan(0xffff4444));
 		parseSimpleTag(ssb, text, CODE, new TypefaceSpan("monospace"));
 		parseSimpleTag(ssb, text, PRE, new TypefaceSpan("monospace"));
+		parseParamterizedTag(ssb, text, ALIGN, new AlignTag());
 		return ssb;
 	}
 
@@ -48,6 +50,61 @@ public class WhatBBParser {
 			e -= tag[0].length();
 			ssb.delete(e, e + tag[1].length());
 			text.delete(e, e + tag[1].length());
+		}
+	}
+
+	/**
+	 * Parse parameterized tags in the text and apply the corresponding styles returned by the ParameterizedTag handler
+	 * Tag openers should be of the form '[tag=' to make finding the parameter simple
+	 *
+	 * @param ssb     spannable string builder to apply the styling in
+	 * @param text    a mirror of the text in the spannable string builder, used to look up tag positions and tags will be
+	 *                removed in here and in the ssb to keep them matching
+	 * @param tag     tag[0] is the opening tag, tag[1] is the closing tag
+	 * @param handler handler to return the appropriate tag for the parameters
+	 */
+	private static void parseParamterizedTag(SpannableStringBuilder ssb, StringBuilder text, String tag[], ParameterizedTag handler){
+		for (int s = text.indexOf(tag[0]) + tag[0].length(), e = text.indexOf(tag[1], s); s != -1 && e != -1;
+			 s = text.indexOf(tag[0], s) + tag[0].length(), e = text.indexOf(tag[1], s)){
+			//Find the tag parameter
+			int openerClose = text.indexOf("]", s);
+			String param = text.substring(s, openerClose);
+			ssb.setSpan(handler.getStyle(param, text.substring(openerClose + 1, e)), s, e, 0);
+			//Remove the open and close tokens
+			ssb.delete(s - tag[0].length(), openerClose + 1);
+			text.delete(s - tag[0].length(), openerClose + 1);
+			e -= param.length() + tag[0].length() + 1;
+			ssb.delete(e, e + tag[1].length());
+			text.delete(e, e + tag[1].length());
+		}
+	}
+
+	/**
+	 * An interface for creating various types of parameterized tags. The object returned
+	 * must be some kind of Character or Paragraph style
+	 */
+	public static interface ParameterizedTag {
+		/**
+		 * Parse the parameters for the tag (and optionally the text effected) and
+		 * return the appropriate tag
+		 *
+		 * @param param tag parameters
+		 * @param text  text effected by the tag
+		 * @return style to apply to the effected text
+		 */
+		Object getStyle(String param, String text);
+	}
+
+	private static class AlignTag implements ParameterizedTag {
+		@Override
+		public Object getStyle(String param, String text){
+			if (param.equalsIgnoreCase("left")){
+				return new AlignmentSpan.Standard(Layout.Alignment.ALIGN_NORMAL);
+			}
+			else if (param.equalsIgnoreCase("center")){
+				return new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER);
+			}
+			return new AlignmentSpan.Standard(Layout.Alignment.ALIGN_OPPOSITE);
 		}
 	}
 }
