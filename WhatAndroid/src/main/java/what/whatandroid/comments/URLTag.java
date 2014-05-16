c791d186d087a0d48753db39bf6e9a18a70083c3
Will <willusher.life@gmail.com>
2014-05-16 16:55:45 -0700

    Fix handling of abbreviated internal site links
    For example the site allows links to be posted as just artist.php?id=x
    but to pick these up with our intent filters we need the full site link
    so expand it back out. I think these are the only links that will
    not be starting with http so it should be fine.

--- WhatAndroid/src/main/java/what/whatandroid/comments/URLTag.java
+++ WhatAndroid/src/main/java/what/whatandroid/comments/URLTag.java
@@ -4,6 +4,7 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.URLSpan;
+import api.soup.MySoup;
 
 /**
  * Returns the appropriate URLSpan for the url tag
@@ -12,12 +13,18 @@ public class URLTag implements ParameterizedTag {
 	@Override
 	public Spannable getStyle(String param, String text){
 		SpannableString styled = new SpannableString(text);
+		String url;
 		if (param != null && !param.isEmpty()){
-			styled.setSpan(new URLSpan(param), 0, styled.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+			url = param;
 		}
 		else {
-			styled.setSpan(new URLSpan(text), 0, styled.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+			url = text;
 		}
+		//TODO: Is it safe to assume all urls without the http bit will be site urls?
+		if (!url.startsWith("http")){
+			url = MySoup.getSite() + url;
+		}
+		styled.setSpan(new URLSpan(url), 0, styled.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 		return styled;
 	}
 }
