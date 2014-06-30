b845832d820f5a6595d4611d2b5f3db8331161cb
Will <willusher.life@gmail.com>
2014-06-30 14:53:13 -0700

    Save/restore scroll state for forum pages as well

--- WhatAndroid/src/main/java/what/whatandroid/forums/forum/ForumListFragment.java
+++ WhatAndroid/src/main/java/what/whatandroid/forums/forum/ForumListFragment.java
@@ -1,6 +1,7 @@
 package what.whatandroid.forums.forum;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
 import api.forum.forum.Forum;
 import api.soup.MySoup;
 import what.whatandroid.R;
@@ -21,9 +23,14 @@ import what.whatandroid.forums.ForumActivity;
  * Fragment that displays a list of the forum threads on some page of the forum
  */
 public class ForumListFragment extends Fragment implements OnLoggedInCallback, LoaderManager.LoaderCallbacks<Forum> {
+	//Used to save/restore our scroll position in the forum page view
+	private static final String SCROLL_STATE = "what.whatandroid.forumlistfragment.SCROLL_STATE";
+
 	private LoadingListener<Forum> listener;
 	private ProgressBar loadingIndicator;
+	private ListView list;
 	private ForumListAdapter adapter;
+	private Parcelable scrollState;
 
 	/**
 	 * Get a fragment displaying the list of posts at some page in the forum
@@ -45,9 +52,17 @@ public class ForumListFragment extends Fragment implements OnLoggedInCallback, L
 	}
 
 	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		if (savedInstanceState != null){
+			scrollState = savedInstanceState.getParcelable(SCROLL_STATE);
+		}
+	}
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View view = inflater.inflate(R.layout.fragment_list_view, container, false);
-		ListView list = (ListView)view.findViewById(R.id.list);
+		list = (ListView) view.findViewById(R.id.list);
 		loadingIndicator = (ProgressBar)view.findViewById(R.id.loading_indicator);
 		adapter = new ForumListAdapter(getActivity());
 		list.setAdapter(adapter);
@@ -56,6 +71,12 @@ public class ForumListFragment extends Fragment implements OnLoggedInCallback, L
 			getLoaderManager().initLoader(0, getArguments(), this);
 		}
 		return view;
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState){
+		super.onSaveInstanceState(outState);
+		outState.putParcelable(SCROLL_STATE, list.onSaveInstanceState());
 	}
 
 	/**
@@ -90,6 +111,9 @@ public class ForumListFragment extends Fragment implements OnLoggedInCallback, L
 				adapter.notifyDataSetChanged();
 				if (listener != null){
 					listener.onLoadingComplete(data);
+				}
+				if (scrollState != null){
+					list.onRestoreInstanceState(scrollState);
 				}
 			}
 		}
