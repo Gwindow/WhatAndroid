111154f542b1d94ceff8d9135e4534e85375a4a0
Will <willusher.life@gmail.com>
2014-07-21 18:13:45 -0700

    Add custom layout for conversations in the inbox

--- WhatAndroid/src/main/java/what/whatandroid/inbox/InboxListAdapter.java
+++ WhatAndroid/src/main/java/what/whatandroid/inbox/InboxListAdapter.java
@@ -1,12 +1,16 @@
 package what.whatandroid.inbox;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Date;
 
 import api.inbox.inbox.Message;
 import what.whatandroid.R;
@@ -20,7 +24,7 @@ public class InboxListAdapter extends ArrayAdapter<Message> implements AdapterVi
 	private ViewConversationCallbacks viewConversation;
 
 	public InboxListAdapter(Context context){
-		super(context, R.layout.list_group);
+		super(context, R.layout.list_message);
 		inflater = LayoutInflater.from(context);
 		try {
 			viewConversation = (ViewConversationCallbacks)context;
@@ -37,13 +41,33 @@ public class InboxListAdapter extends ArrayAdapter<Message> implements AdapterVi
 			holder = (ViewHolder)convertView.getTag();
 		}
 		else {
-			convertView = inflater.inflate(R.layout.list_group, parent, false);
+			convertView = inflater.inflate(R.layout.list_message, parent, false);
 			holder = new ViewHolder();
-			holder.subject = (TextView)convertView.findViewById(R.id.group_category);
+			holder.subject = (TextView)convertView.findViewById(R.id.subject);
+			holder.sender = (TextView)convertView.findViewById(R.id.sender);
+			holder.date = (TextView)convertView.findViewById(R.id.date);
+			holder.sticky = (ImageView)convertView.findViewById(R.id.sticky_thread);
 			convertView.setTag(holder);
 		}
 		Message message = getItem(position);
 		holder.subject.setText(message.getSubject());
+		holder.sender.setText("Sender: " + message.getUsername());
+		holder.date.setText(DateUtils.getRelativeTimeSpanString(message.getDate().getTime(),
+			new Date().getTime(), 0, DateUtils.FORMAT_ABBREV_ALL));
+
+		if (message.isUnread()){
+			holder.subject.setTextColor(getContext().getResources().getColor(android.R.color.primary_text_dark));
+		}
+		else {
+			holder.subject.setTextColor(getContext().getResources().getColor(android.R.color.secondary_text_dark));
+		}
+
+		if (message.isSticky()){
+			holder.sticky.setVisibility(View.VISIBLE);
+		}
+		else {
+			holder.sticky.setVisibility(View.GONE);
+		}
 		return convertView;
 	}
 
@@ -54,6 +78,7 @@ public class InboxListAdapter extends ArrayAdapter<Message> implements AdapterVi
 	}
 
 	private static class ViewHolder {
-		public TextView subject;
+		public TextView subject, sender, date;
+		public ImageView sticky;
 	}
 }
