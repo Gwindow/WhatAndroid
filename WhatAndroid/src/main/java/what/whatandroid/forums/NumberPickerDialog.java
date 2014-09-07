64c2bc6d5584ae785e2a5861c8a5a0b9de4df434
Kryštof Šplíchal <krystofspl@gmail.com>
2014-09-07 09:21:03 +0200

    Fixed NumberPickerDialog

--- WhatAndroid/src/main/java/what/whatandroid/forums/NumberPickerDialog.java
+++ WhatAndroid/src/main/java/what/whatandroid/forums/NumberPickerDialog.java
@@ -21,43 +21,43 @@ import what.whatandroid.R;
  * onActivityResultCallback which should implement the NumberPickerListener
  */
 public class NumberPickerDialog extends DialogFragment {
-	/**
-	 * Interface to receive the picked number
-	 */
-	public interface NumberPickerListener {
-		/**
-		 * Called when a number is picked
-		 *
-		 * @param number the chosen number
-		 */
-		public void pickNumber(int number);
-	}
+    /**
+     * Interface to receive the picked number
+     */
+    public interface NumberPickerListener {
+        /**
+         * Called when a number is picked
+         *
+         * @param number the chosen number
+         */
+        public void pickNumber(int number);
+    }
 
-	public static final String MIN = "what.whatandroid.numberpickerdialog.MIN",
-		MAX = "what.whatandroid.numberpickerdialog.MAX",
-		TITLE = "what.whatandroid.numberpickerdialog.TITLE",
-        VALUE = "what.whatandroid.numberpickerdialog.VALUE";
+    public static final String MIN = "what.whatandroid.numberpickerdialog.MIN",
+            MAX = "what.whatandroid.numberpickerdialog.MAX",
+            TITLE = "what.whatandroid.numberpickerdialog.TITLE",
+            VALUE = "what.whatandroid.numberpickerdialog.VALUE";
 
-	private NumberPickerListener listener;
+    private NumberPickerListener listener;
 
-	/**
-	 * Create a NumberPickerDialog to pick values in the range [min, max]
-	 * and with the desired title
-	 *
-	 * @param title title for the dialog
-	 * @param min   min value that should be selectable
-	 * @param max   max value that should be selectable
-	 */
-	public static NumberPickerDialog newInstance(String title, int min, int max){
-		NumberPickerDialog d = new NumberPickerDialog();
-		Bundle args = new Bundle();
-		args.putString(TITLE, title);
-		args.putInt(MIN, min);
-		args.putInt(MAX, max);
+    /**
+     * Create a NumberPickerDialog to pick values in the range [min, max]
+     * and with the desired title
+     *
+     * @param title title for the dialog
+     * @param min   min value that should be selectable
+     * @param max   max value that should be selectable
+     */
+    public static NumberPickerDialog newInstance(String title, int min, int max) {
+        NumberPickerDialog d = new NumberPickerDialog();
+        Bundle args = new Bundle();
+        args.putString(TITLE, title);
+        args.putInt(MIN, min);
+        args.putInt(MAX, max);
         args.putInt(VALUE, 1); // default page is 1
-		d.setArguments(args);
-		return d;
-	}
+        d.setArguments(args);
+        return d;
+    }
 
     /**
      * Create a NumberPickerDialog to pick values in the range [min, max]
@@ -68,59 +68,53 @@ public class NumberPickerDialog extends DialogFragment {
      * @param max   max value that should be selectable
      * @param value initial value of the dialog
      */
-    public static NumberPickerDialog newInstance(String title, int min, int max, int value){
-        NumberPickerDialog d = newInstance(title,min,max);
-        d.getArguments().remove(VALUE);
-        d.getArguments().putInt(VALUE,value);
+    public static NumberPickerDialog newInstance(String title, int min, int max, int value) {
+        NumberPickerDialog d = newInstance(title, min, max);
+        d.getArguments().putInt(VALUE, value);
         return d;
     }
 
-	public NumberPickerDialog(){
-		//Required empty ctor
-	}
+    public NumberPickerDialog() {
+        //Required empty ctor
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		try {
-			listener = (NumberPickerListener)getTargetFragment();
-		}
-		catch (ClassCastException e){
-			throw new ClassCastException(getTargetFragment().toString() + " must implement NumberPickerListener");
-		}
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            listener = (NumberPickerListener) getTargetFragment();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(getTargetFragment().toString() + " must implement NumberPickerListener");
+        }
+    }
 
-	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState){
-		AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(),
-			android.R.style.Theme_Holo_Dialog_MinWidth));
-		LayoutInflater inflater = LayoutInflater.from(getActivity());
-		View view = inflater.inflate(R.layout.dialog_number_picker, null);
-		final NumberPicker picker = (NumberPicker)view.findViewById(R.id.number_picker);
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(),
+                android.R.style.Theme_Holo_Dialog_MinWidth));
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View view = inflater.inflate(R.layout.dialog_number_picker, null);
+        final NumberPicker picker = (NumberPicker) view.findViewById(R.id.number_picker);
 
-		Bundle args = getArguments();
-		picker.setMinValue(args.getInt(MIN));
-		picker.setMaxValue(args.getInt(MAX));
+        Bundle args = getArguments();
+        picker.setMinValue(args.getInt(MIN));
+        picker.setMaxValue(args.getInt(MAX));
         picker.setValue(args.getInt(VALUE));
 
-		builder.setView(view)
-			.setTitle(args.getString(TITLE))
-			.setPositiveButton("Select", new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which){
-					listener.pickNumber(picker.getValue());
-				}
-			})
-			.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which){
-					dialog.cancel();
-				}
-			});
-		return builder.create();
-	}
-
-    public void setValue(int value){
-
+        builder.setView(view)
+                .setTitle(args.getString(TITLE))
+                .setPositiveButton("Select", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listener.pickNumber(picker.getValue());
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        return builder.create();
     }
 }
